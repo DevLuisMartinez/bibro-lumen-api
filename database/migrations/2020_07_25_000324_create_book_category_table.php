3b713daa0ac7dc9615f853ee27e128c06edb7b42
Luis Martinez <devluismartinezr@gmail.com>
2020-07-24 19:35:43 -0500

    migrations db (books, categories)

---
+++ database/migrations/2020_07_25_000324_create_book_category_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_category', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('book_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('books')->on('id');
+            $table->foreign('category_id')->references('categories')->on('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_category');
+    }
+}
