a4f9075eeac46ac7d1b092e91a147d69a763c9d8
Luis Martinez <devluismartinezr@gmail.com>
2020-07-25 23:56:08 -0500

    book and category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+
 class Category extends Model {
 
-    protected $table = 'category';
+    protected $table = 'categories';
+    protected $fillable = ['name','description','icon'];
 
     public function books(){
         return belognsToMany(Book::class, 'book_category', 'category_id', 'libro_id');
